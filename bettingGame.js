15ebbaef972af68fa1661576410466be9b89342c
Nicholas Gust <nicholasgust@gmail.com>
2016-02-08 20:28:29 -0800

    Program works, needs some improvment, and style

--- bettingGame.js
+++ bettingGame.js
@@ -10,18 +10,13 @@ $(document).ready(function(){
 
     var compare = function(num, guess, wager) {
       if (guess == num) {
-        console.log("You're right");
         $(".feedback").text("You're right");
         bank = bank + wager;
-        console.log("You win $" + wager);
         $(".feedback").text("You win $" + wager);
       } else if (Math.abs(guess - num) <= 1) {
-        console.log("You're close");
         $(".feedback").text("You're close");
-        console.log("No money lost");
         $(".dollars").text("No money lost");
       } else {
-        console.log("Way off!");
         bank = bank - wager;
         console.log("You lose $" + wager);
         $(".dollars").text("You lose $" + wager);
@@ -30,14 +25,11 @@ $(document).ready(function(){
     }
     var game = function() {
         var num = Math.round(Math.random()*10);
-        // var wager = prompt("How much do you want to wager?");
-        alert("How much do you want to wager?");
         var wager = parseInt($("#bet_ammount").val(),10);
         if (bank - wager < 0) { alert("You can't bet more than you have."); return gameOver(); }
-        // var guess = prompt("Guess a number between 1 and 10");
+
         var guess = parseInt($("#guess").val(),10);
         $(".guess").text("Your guess is " + guess);
-        console.log("The number is " + num);
         $(".answer").text("The number is " + num);
 
         compare(num, guess, wager);
@@ -46,7 +38,6 @@ $(document).ready(function(){
       // while(userAnswer === "yes" && bank >= 0) {
            game();
           $(".money").text("You now have $" + bank + " dollars");
-          userAnswer = prompt("Want to bet again?");
       // };
     });
 
