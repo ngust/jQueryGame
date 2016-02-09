983c4fbe7adc67d717ca9c3cee1ae3d17ccfbf1b
Nicholas Gust <nicholasgust@gmail.com>
2016-02-08 20:17:50 -0800

    button works, page kinda works, needs work

--- bettingGame.js
+++ bettingGame.js
@@ -32,21 +32,23 @@ $(document).ready(function(){
         var num = Math.round(Math.random()*10);
         // var wager = prompt("How much do you want to wager?");
         alert("How much do you want to wager?");
-        var bet = parseInt($("#bet_ammount").val(),10);
+        var wager = parseInt($("#bet_ammount").val(),10);
         if (bank - wager < 0) { alert("You can't bet more than you have."); return gameOver(); }
-        var guess = prompt("Guess a number between 1 and 10");
+        // var guess = prompt("Guess a number between 1 and 10");
+        var guess = parseInt($("#guess").val(),10);
         $(".guess").text("Your guess is " + guess);
         console.log("The number is " + num);
         $(".answer").text("The number is " + num);
 
         compare(num, guess, wager);
     };
-
-    while(userAnswer === "yes" && bank >= 0) {
-         game();
-        $(".money").text("You now have $" + bank + " dollars");
-        userAnswer = prompt("Want to bet again?");
-    };
+    $('#make-bet').on('click', function() {
+      // while(userAnswer === "yes" && bank >= 0) {
+           game();
+          $(".money").text("You now have $" + bank + " dollars");
+          userAnswer = prompt("Want to bet again?");
+      // };
+    });
 
     var gameOver = function() {
       console.log("You now have $" + bank + " dollars");
